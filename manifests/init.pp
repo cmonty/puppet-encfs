81fb3a52509830cd9c7c72a6271fa47c46d41c81
cory <cory.monty@gmail.com>
2013-03-03 16:05:04 -0600

    fixes typo

--- manifests/init.pp
+++ manifests/init.pp
@@ -14,7 +14,7 @@ class encfs {
   }
 
   exec { "copy-fuse4x":
-    command => "sudo /bin/cp -rfX /opt/boxen/homebrew/Cellar/fuse4x-kext/0.9.2/Library/Extensions/fuse4x.kext /Library/Extensions":
+    command => "sudo /bin/cp -rfX /opt/boxen/homebrew/Cellar/fuse4x-kext/0.9.2/Library/Extensions/fuse4x.kext /Library/Extensions",
     require => Package["encfs"],
     creates => "/Library/Extensions/fuse4x.kext"
   }
