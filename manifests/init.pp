c59e0e0db68d5e46274570ef45bce83972e073bb
Cory Monty <cory.monty@gmail.com>
2013-03-03 15:14:07 -0600

    adds fuse4x commands to sudoers file

--- manifests/init.pp
+++ manifests/init.pp
@@ -3,6 +3,16 @@ class encfs {
 
   package { "encfs": }
 
+  sudoers { "encfs":
+    users => $::luser,
+    hosts => "ALL",
+    commands => [
+      "/bin/cp -rfX /opt/boxen/homebrew/Cellar/fuse4x-kext/0.9.2/Library/Extensions/fuse4x.kext /Library/Extensions",
+      "/bin/chmod +s /Library/Extensions/fuse4x.kext/Support/load_fuse4x"
+    ],
+    type => "user_spec"
+  }
+
   exec { "sudo /bin/cp -rfX /opt/boxen/homebrew/Cellar/fuse4x-kext/0.9.2/Library/Extensions/fuse4x.kext /Library/Extensions":
     require => Package["encfs"],
   }
